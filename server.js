20cf52557c3a70b8951af936dd9a13479808a0a9
Adam Olson <adam.lee.olson@outlook.com>
2023-08-27 15:00:17 -0500

    still working on the server.js

--- server.js
+++ server.js
@@ -7,3 +7,24 @@ const connection = mysql.createConnection({
     database: 'EMS'
 
 });
+
+
+const init = async () => {
+
+    const results = await connection.promise().query(
+`INSERT INTO employee (first_name, last_name, title, department, salary, manager)
+VALUES 
+('Leonardo', 'Turtle', 'Ninja', 'Sewers', '2000.00', 'splinter')`,
+
+    )
+
+const department = await connection.promise().query
+
+('SELECT * FROM department')
+
+
+console.log(department)
+ 
+}
+
+init()
