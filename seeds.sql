dffe5f46122f673dc974038c86adefcf8cbad19f
Adam Olson <adam.lee.olson@outlook.com>
2023-08-27 14:36:08 -0500

    connecting to the databse and adjustments to the insert tables

--- seeds.sql
+++ seeds.sql
@@ -1,33 +1,33 @@
 -- Insert sample departments
 INSERT INTO department (name) VALUES 
-    ('Game Show Host'),
-    ('unemployed'),
-    ('not taking no for an answer'),
-    ('actorish'),
-    ('ass kicker'),
-    ('caretaker'),
-    ('Spaceman'),
-    ('Most powerful woman in the Universe');
+    ('Game Show Host Department'),
+    ('Unemployment Office'),
+    ('Resistance Against No Department'),
+    ('Dramatic Arts Department'),
+    ('Kick-Ass Division'),
+    ('Caregivers Guild'),
+    ('Space Exploration Department'),
+    ('Most Dominant Universe Management Team');
 
--- Insert sample roles
+-- Insert sample roles with unique and humorous titles
 INSERT INTO role (title, salary, department_id) VALUES
-    ('Game Show Host', 100000.00, 1),
-    ('unemployed', 0.00, 2),
-    ('not taking no for an answer', 50000.00, 3),
-    ('actorish', 75000.00, 4),
-    ('ass kicker', 90000.00, 5),
-    ('caretaker', 40000.00, 6),
-    ('Spaceman', 120000.00, 7),
-    ('Most powerful woman in the Universe', 150000.00, 8);
+    ('Master of Sales Shenanigans', 100000.00, 1),
+    ('Professional Handshaker', 0.00, 2),
+    ('Coding Wizard', 50000.00, 3),
+    ('Dramatic Thespian', 75000.00, 4),
+    ('Ninja of Deals', 90000.00, 5),
+    ('Chief Snack Officer', 40000.00, 6),
+    ('Intergalactic Explorer', 120000.00, 7),
+    ('Supreme Empress of the Multiverse', 150000.00, 8);
 
 -- Insert sample employees
 INSERT INTO employee (first_name, last_name, title, department, salary, manager)
 VALUES
-    ('Bob', 'Barker', 'Game Show Host', 'Game Show Host', 100000.00, NULL),
-    ('Phil', 'Mckcrackin', 'unemployed', 'unemployed', 0.00, NULL),
-    ('Tommy', 'Boy', 'not taking no for an answer', 'not taking no for an answer', 50000.00, NULL),
-    ('Burt', 'Reynolds', 'actorish', 'actorish', 75000.00, NULL),
-    ('Chuck', 'Norris', 'ass kicker', 'ass kicker', 90000.00, NULL),
-    ('Joe', 'Mamma', 'caretaker', 'caretaker', 40000.00, NULL),
-    ('Neil', 'Armstrong', 'Spaceman', 'Spaceman', 120000.00, NULL),
-    ('She-ra', '', 'Most powerful woman in the Universe', 'Most powerful woman in the Universe', 150000.00, NULL);
+    ('Bob', 'Barker', 'Master of Sales Shenanigans', 'Game Show Host Department', 100000.00, NULL),
+    ('Phil', 'Mckcrackin', 'Professional Handshaker', 'Unemployment Office', 0.00, NULL),
+    ('Tommy', 'Boy', 'Coding Wizard', 'Resistance Against No Department', 50000.00, NULL),
+    ('Burt', 'Reynolds', 'Dramatic Thespian', 'Dramatic Arts Department', 75000.00, NULL),
+    ('Chuck', 'Norris', 'Ninja of Deals', 'Kick-Ass Division', 90000.00, NULL),
+    ('Joe', 'Mamma', 'Chief Snack Officer', 'Caregivers Guild', 40000.00, NULL),
+    ('Neil', 'Armstrong', 'Intergalactic Explorer', 'Space Exploration Department', 120000.00, NULL),
+    ('She-ra', 'Princess', 'Supreme Empress of the Multiverse', 'Most Dominant Universe Management Team', 150000.00, NULL);
